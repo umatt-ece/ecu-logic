955a2511b91926b7ddadd11d775315dbfa3eef14
Bruce <97542494+BruceNiemi@users.noreply.github.com>
2025-07-14 21:50:37 -0500

    chore: create a basic skeleton

---
+++ include/ecu_logic.h
@@ -0,0 +1,14 @@
+#ifndef ECU_LOGIC_H
+#define ECU_LOGIC_H
+
+typedef struct ecu_logic_state {
+} ecu_logic_state_t;
+
+/**
+ * Initializes the ECU logic state.
+ *
+ * @param state Pointer to the ECU logic state structure to initialize.
+ */
+void ecu_logic_init(ecu_logic_state_t *state);
+
+#endif  // ECU_LOGIC_H
